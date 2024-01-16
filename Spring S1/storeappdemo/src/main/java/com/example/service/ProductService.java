78c813f5729f85caccb0afb8ff14461f37b1cf8a
Rahul Mandal <rahulbokaro610@gmail.com>
2024-01-16 20:03:36 +0530

    updated

--- Spring S1/storeappdemo/src/main/java/com/example/service/ProductService.java
+++ Spring S1/storeappdemo/src/main/java/com/example/service/ProductService.java
@@ -1,6 +1,7 @@
 package com.example.service;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -14,15 +15,15 @@ import com.example.repository.ProductRepository;
 @Service("productService")
 @Scope("singleton")
 @Transactional
-public class ProductService implements IProductService{
+public class ProductService implements IProductService {
 
 	@Autowired
 	@Qualifier("productRepository")
 	private ProductRepository productRepository;
-	
+
 	@Override
 	public Product save(Product product) {
-		
+
 		return productRepository.save(product);
 	}
 
@@ -33,37 +34,47 @@ public class ProductService implements IProductService{
 	}
 
 	@Override
+	public void patchProduct(Map<String, Object> updates, int id) {
+
+		Product dbProduct = productRepository.findById(id).get();
+
+		Double price = (Double) updates.get("price");
+		if (price != null) {
+			dbProduct.setPrice(price);
+			productRepository.save(dbProduct);
+		}
+	}
+
+	@Override
 	public void delete(int id) {
 
-		 productRepository.deleteById(id);
+		productRepository.deleteById(id);
 	}
 
 	@Override
 	@Transactional(readOnly = true)
 	public Product getProductById(int id) {
-	
+
 		return productRepository.findById(id).get();
 	}
 
 	@Override
 	@Transactional(readOnly = true)
 	public List<Product> getAllProducts() {
-		
+
 		return productRepository.findAll();
 	}
 
 	@Override
 	public List<Product> getAllProductsByName(String name) {
-		
-		return productRepository.findByName(
-				"%" + name + "%");
+
+		return productRepository.findByName("%" + name + "%");
 	}
 
 	@Override
 	public List<Product> getAllProductsByPrice(Double price) {
-		
+
 		return productRepository.findByPrice(price);
 	}
 
-	
 }
