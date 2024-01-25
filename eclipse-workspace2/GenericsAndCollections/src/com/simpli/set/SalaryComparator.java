a281a31e63c84dcb8130c18bda377f6b5d550ba4
Rahul Mandal <rahulbokaro610@gmail.com>
2024-01-25 12:29:27 +0530

    set comparable&Comparator

---
+++ eclipse-workspace2/GenericsAndCollections/src/com/simpli/set/SalaryComparator.java
@@ -0,0 +1,14 @@
+package com.simpli.set;
+
+import java.util.Comparator;
+
+public class SalaryComparator implements Comparator<Employee>{
+
+	@Override
+	public int compare(Employee emp1, Employee emp2) {
+		
+		return (int) (emp1.getSalary() - emp2.getSalary());
+	}
+	
+
+}
